a44c8ae200c72fd2e81a30b816f1b373d127d426
Viktoriia Kucher <142267781+torry455@users.noreply.github.com>
2024-01-20 14:50:21 +0200

    changed task-2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,42 +1,45 @@
 const form = document.querySelector('.feedback-form');
-const inputEmail = form.querySelector('input');
-const inputText = form.querySelector('textarea');
+const inputEmail = form.querySelector('input[name="email"]');
+const inputText = form.querySelector('textarea[name="message"]');
 
 const currentObj = {
-    email:'',
-    message:'',
-} 
-let localStorageData = '';
+    email: '',
+    message: '',
+};
+
 const localStorageKey = "feedback-form-state";
+let localStorageData = '';
+
 try {
-    localStorageData = JSON.parse(localStorage.getItem(localStorageKey)) ?? {};
+    localStorageData = JSON.parse(localStorage.getItem(localStorageKey)) || {};
     
     if (localStorageData && typeof localStorageData === 'object') {
-        currentObj.email = localStorageData.email ||'';
+        currentObj.email = localStorageData.email || '';
         currentObj.message = localStorageData.message || '';
         inputEmail.value = currentObj.email;
         inputText.value = currentObj.message;
-}
-} catch(error) {
-console.error('Error parsing JSON:', error);
+    }
+} catch (error) {
+    console.error('Error parsing JSON:', error);
 }
 
 const upLoadNewDate = (event) => {
     const target = event.target;
     if (target === inputEmail) {
-        currentObj.email = target.value;
-    } else if(target === inputText) {
-        currentObj.message = target.value;
+        currentObj.email = target.value.trim();
+    } else if (target === inputText) {
+        currentObj.message = target.value.trim();
     }
     localStorageData = JSON.stringify(currentObj);
-    localStorage.setItem(localStorageKey,localStorageData)
+    localStorage.setItem(localStorageKey, localStorageData);
 };
-form.addEventListener('input',upLoadNewDate)
+
+form.addEventListener('input', upLoadNewDate);
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     if (inputEmail.value.trim() === "" || inputText.value.trim() === '') {
-        alert(`Будь ласка, заповніть всі поля форми перед відправленням.`);
+        alert(`Please fill in all form fields before submitting.`);
         return;
     }
     console.log(JSON.parse(localStorage.getItem(localStorageKey)));
@@ -44,4 +47,4 @@ form.addEventListener('submit', (event) => {
     currentObj.email = '';
     currentObj.message = '';
     form.reset();
-})
+});
